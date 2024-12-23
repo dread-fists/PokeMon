4f96b49d0f795e5dd0b75bbd0b1dbcb050e35956
dread-fists <78465283+dread-fists@users.noreply.github.com>
2024-12-23 12:16:37 +0900

    Update card.js
    Adding sources for card flip

--- resources/src/card.js
+++ resources/src/card.js
@@ -5,14 +5,21 @@ var container=document.querySelector('.container');
 var card=document.querySelector('.card');
 var overlay=document.querySelector('.overlay');
 card.style='background-image:url(../images/card'+cardsel+'.png);';
+container.addEventListener('click',()=>{
+    container.style='transform:rotateY(180deg);';
+    setTimeout(() => {
+        location.href="select.html";
+    }, 300);
+    
+});
 container.addEventListener('mousemove',event=>{
     var x=event.offsetX;
     var y=event.offsetY;
     var rotateY=-20+(4/29)*x;
     var rotateX=20-(4/41)*y;
-    overlay.style='background-position:'+(x/5+y/5)+'%';
+    overlay.style='background-position:'+(x/5+y/5)+'%;';
     container.style='transform : scale(1.2) rotateY('+rotateY+'deg) rotateX('+rotateX+'deg)'
-})
+});
 container.addEventListener('mouseout',()=>{
     container.style='transform : scale(1.0) '
-})
+});
